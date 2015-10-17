bb4dfe887640a5a9f14a5ddd5b27bdb01e6ef4a0
WeiZhen <zhenw@WeiZhens-MacBook-Air.local>
2015-10-17 03:13:29 -0500

    add sign in

--- public/javascripts/magister/magister.js
+++ public/javascripts/magister/magister.js
@@ -2,8 +2,8 @@
 var current_item = 0;
 
 // few settings
-var section_hide_time = 1300;
-var section_show_time = 1300;
+var section_hide_time = 700;
+var section_show_time = 700;
 
 // jQuery stuff
 jQuery(document).ready(function($) {
@@ -19,8 +19,29 @@ jQuery(document).ready(function($) {
 				$(current_item).addClass( 'active' );
 				var new_section = $( $(current_item).attr('href') );
 				new_section.fadeIn( section_show_time );
-			} );
+			});
 		}
 		return false;
+	});	
+
+	$("a", '.section_active_button').click(function() 
+	{
+		current_item = this;
+		// close all visible divs with the class of .section
+		$('.section:visible').fadeOut( section_hide_time, function() {
+			$('a', '.mainmenu').removeClass( 'active' );  
+			var new_section = $( $(current_item).attr('href') );
+			new_section.fadeIn( section_show_time );
+		});
+	});
+
+	$('.cancel').click(function() 
+	{
+		current_item = this;
+		// close all visible divs with the class of .section
+		$('.section:visible').fadeOut( section_hide_time, function() {
+			var new_section = $( $(current_item).attr('href') );
+			new_section.fadeIn( section_show_time );
+		});
 	});		
 });
