298be8626960f0d5354cc8053581018a38d47304
WeiZhen <zhenw@WeiZhens-MacBook-Air.local>
2015-10-19 00:34:09 -0400

    Revert "..."
    This reverts commit 411bd72c54b39181ee764702c08a9561d26d7e96.

--- public/javascripts/AngularJS/controllers/SignInController.js
+++ public/javascripts/AngularJS/controllers/SignInController.js
@@ -28,5 +28,9 @@ angularApp.controller('SignInController', ['$scope', '$firebaseObject'
 			$(".login-action-hidden").removeClass("hidden");
 			$(".login-action").addClass("hidden");
 		};
+
+		$scope.register = function() {
+			console.log('User clicked register', $scope.user);
+		};
 	}
 ]);
