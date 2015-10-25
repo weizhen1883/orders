0c8cfdc7fcd87748e346c4fb6fa80a8fde24dfa3
WeiZhen <zhenw@WeiZhens-MacBook-Air.local>
2015-10-24 21:39:31 -0400

    start working on orders

--- public/javascripts/AngularJS/controllers/OrderController.js
+++ public/javascripts/AngularJS/controllers/OrderController.js
@@ -1,5 +1,30 @@
-angularApp.controller('SignInController', ['$scope',
+angularApp.controller('OrderController', ['$scope',
 	function($scope) {
+		$scope.order = {
+			zoon: '',
+			pickup_location: '',
+			orders: {
+				entress: '',
+				sides: '',
+				rice_noodle: ''
+			}
+		};
+
+		$scope.location_select = function(zoon, pickup_location) {
+			$scope.order.zoon = zoon;
+			$scope.order.pickup_location = pickup_location;
+			console.log($scope.order);
+		};
+
+		$scope.entress_select = function(entress) {
+			$scope.order.orders.entress = entress;
+			console.log($scope.order);
+		};
+
+		$scope.sides_select = function(side1, side2) {
+			$scope.order.orders.sides = {side1, side2};
+			console.log($scope.order);
+		};
 
 	}
 ]);
