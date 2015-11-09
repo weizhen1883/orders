72a2ee7da78f48e5d8e91897d17ec945f102e9ba
WeiZhen <zhenw@WeiZhens-MacBook-Air.local>
2015-11-09 01:19:38 -0500

    fix the url

--- public/javascripts/AngularJS/services/MenuServices.js
+++ public/javascripts/AngularJS/services/MenuServices.js
@@ -1,5 +1,5 @@
 angularApp.factory('MenuItems', ['$http', function($http){
-	return $http.post('/getMenu', {zone: 'UIC'}).success(function(data) {
+	return $http.post('/orders/getMenu', {zone: 'UIC'}).success(function(data) {
 	    return data;
 	}).error(function(err) {
 	    return err;
